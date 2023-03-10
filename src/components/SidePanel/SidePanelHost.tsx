26a0eafc24e95f41be769b22fc27c4ef260d8d30
Jeffrey Phillips <jephilli@redhat.com>
2023-03-10 14:58:31 -0500

    Fix for side panel transition on first display

--- src/components/SidePanel/SidePanelHost.tsx
+++ src/components/SidePanel/SidePanelHost.tsx
@@ -9,12 +9,22 @@ import SidePanelContext, { SidePanelProps } from './SidePanelContext';
 
 const SidePanelHost: React.FC = ({ children }) => {
   const [props, setProps] = React.useState<SidePanelProps>({ isExpanded: false });
+  const [initDisplay, setInitDisplay] = React.useState<boolean>(true);
 
   const panelContent = <DrawerPanelContent isResizable>{props.children}</DrawerPanelContent>;
 
+  React.useEffect(() => {
+    setTimeout(() => setInitDisplay(false), 300);
+  }, []);
+
   return (
     <SidePanelContext.Provider value={{ setProps }}>
-      <Drawer isExpanded={props.isExpanded} isInline={props.isInline} onExpand={props.onExpand}>
+      <Drawer
+        isExpanded={props.isExpanded}
+        isInline={props.isInline}
+        onExpand={props.onExpand}
+        className={initDisplay ? 'pf-m-resizing' : 'undefined'}
+      >
         <DrawerContent panelContent={panelContent}>
           <DrawerContentBody>{children}</DrawerContentBody>
         </DrawerContent>
